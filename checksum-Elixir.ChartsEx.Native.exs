628d26918d8b8c8e1a723b43cbdcf3aadce0fb60
James Tippett <james.tippett@gmail.com>
2026-03-25 14:42:14 +0700

    chore: add precompiled binary checksums for v0.1.0

---
+++ checksum-Elixir.ChartsEx.Native.exs
@@ -0,0 +1,14 @@
+%{
+  "libcharts_ex-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:95be8098176a785d8b70c1806eebc9de05668a7e9f97034141915a3536df6357",
+  "libcharts_ex-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:5fe7feba0a86dc08301139c61d69a0796cee493c75d70e793439b96d01b8fc41",
+  "libcharts_ex-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:7159e497c17f806776c0f5e075a8b3dd7ae80bc5dce9028a55d31e9e05cb831b",
+  "libcharts_ex-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:3f78601146b0c6ebfb1732b389bdd5f19bc2e7f567b599b1d5ef9c51264da0db",
+  "libcharts_ex-v0.1.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:efbfbac12bbec6b2dc91be1305ba6b035f6bb94fadb37733e67c65735081df34",
+  "libcharts_ex-v0.1.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:924e4d54744c377c8c4fcbca125b2907e6e3b19fd49a78602e39b3b377a034a2",
+  "libcharts_ex-v0.1.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:580e7cd838aa11dfbab4459bc180eaae6a764c5d66a057e2eb2828b0f6e38b09",
+  "libcharts_ex-v0.1.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:dc7574425ede1d2b21319024b5e501c2592a8d9cdc909c697f68c3626dabf183",
+  "libcharts_ex-v0.1.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:dcce00261a39817f735489f6849d0bfe37bfa4ce743fa7d2d08fe30dd0b7d4e2",
+  "libcharts_ex-v0.1.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f86a1cc1e6c78e0348bbd47ed0a96a3b9b1271ae3e87c500a59fa7a881bcaf82",
+  "libcharts_ex-v0.1.0-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:19baa3beb2d27ff71b1d12263115afe0da033817887fb9a80721918a0194ff17",
+  "libcharts_ex-v0.1.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:cd78a7d9f15ec752afa6fa7de4267abc569b2c9dd0f59218a6d3f3327ee7cc12",
+}
