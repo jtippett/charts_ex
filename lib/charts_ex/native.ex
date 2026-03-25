2b8c53f231f9e02d5ca1b8f9aeccab9262982c65
James Tippett <james.tippett@gmail.com>
2026-03-25 12:54:01 +0700

    feat: add Rustler Native module with NIF bridge

---
+++ lib/charts_ex/native.ex
@@ -0,0 +1,21 @@
+defmodule ChartsEx.Native do
+  @moduledoc false
+
+  use RustlerPrecompiled,
+    otp_app: :charts_ex,
+    crate: "charts_ex",
+    base_url:
+      "https://github.com/TODO/charts_ex/releases/download/v#{Mix.Project.config()[:version]}",
+    force_build: System.get_env("CHARTS_EX_BUILD") in ["1", "true"],
+    version: Mix.Project.config()[:version],
+    nif_versions: ["2.17", "2.16", "2.15"],
+    targets: [
+      "aarch64-apple-darwin",
+      "aarch64-unknown-linux-gnu",
+      "x86_64-apple-darwin",
+      "x86_64-unknown-linux-gnu",
+      "x86_64-unknown-linux-musl"
+    ]
+
+  def render(_json), do: :erlang.nif_error(:nif_not_loaded)
+end
---
+++ test/native_test.exs
@@ -0,0 +1,34 @@
+defmodule ChartsEx.NativeTest do
+  use ExUnit.Case, async: true
+
+  test "renders bar chart from JSON" do
+    json = Jason.encode!(%{
+      "type" => "bar",
+      "width" => 600,
+      "height" => 400,
+      "title_text" => "Test",
+      "series_list" => [%{"name" => "A", "data" => [1.0, 2.0, 3.0]}],
+      "x_axis_data" => ["X", "Y", "Z"]
+    })
+
+    assert {:ok, svg} = ChartsEx.Native.render(json)
+    assert svg =~ "<svg"
+  end
+
+  test "returns error for invalid JSON" do
+    assert {:error, msg} = ChartsEx.Native.render("not json")
+    assert msg =~ "invalid JSON"
+  end
+
+  test "returns error for missing type" do
+    json = Jason.encode!(%{"width" => 600})
+    assert {:error, msg} = ChartsEx.Native.render(json)
+    assert msg =~ "type"
+  end
+
+  test "returns error for unknown chart type" do
+    json = Jason.encode!(%{"type" => "nope"})
+    assert {:error, msg} = ChartsEx.Native.render(json)
+    assert msg =~ "unknown chart type"
+  end
+end
